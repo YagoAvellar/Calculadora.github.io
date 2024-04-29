b454ec22474987ab284cfd303abc10e2a9bdf678
Avellar <99830352+YagoAvellar@users.noreply.github.com>
2024-04-29 11:42:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const query = url.parse(req.url, true).query;
     const tabuadaNumber = parseInt(query.tabuada);
+    const sequencia = parseInt(query.sequencia) || 10;
     
     if (!query.tabuada || isNaN(tabuadaNumber)) {
         res.writeHead(400, { 'Content-Type': 'text/plain' });
@@ -12,24 +12,16 @@ const server = http.createServer((req, res) => {
         return;
     }
 
-    let tabuadaHTML = '<h2>Tabuada do ' + tabuadaNumber + '</h2>';
-    for (let i = 1; i <= 10; i++) {
+    let tabuadaHTML = `<h2>Tabuada do ${tabuadaNumber} até ${sequencia}:</h2>`;
+    for (let i = 0; i <= sequencia; i++) {
         tabuadaHTML += `<p>${tabuadaNumber} x ${i} = ${tabuadaNumber * i}</p>`;
     }
 
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Erro interno no servidor.');
-            return;
-        }
-        const updatedHTML = data.replace('{{ tabuada }}', tabuadaHTML);
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(updatedHTML);
-    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(tabuadaHTML);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servidor Calculadora.github.io:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
